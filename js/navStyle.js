7ba3be1dafd8b21ad92ce34c637c0c5bfea26748
zahidulislam2020 <61370815+zahidulislam2020@users.noreply.github.com>
2020-05-18 20:23:07 +0300

    twist-agency

--- js/navStyle.js
+++ js/navStyle.js
@@ -1,38 +1,53 @@
+document.addEventListener('DOMContentLoaded', init);
 
-const header = document.querySelector("header");
-const logo = document.querySelector(".logo-text");
-const navItems = document.querySelector(".nav-links");
-const searchIcon = document.querySelector(".fa-search");
+function init(){
+    let query = window.matchMedia("(max-width: 960px)");
 
-const sectionOne = document.querySelector(".header-section");
+    if(query.matches){
+        //If the page is wider than 575px
+
+    }else{
+        //if the page is narrower or equal to 575px
+        const header = document.querySelector("header");
+        const logo = document.querySelector(".logo-text");
+        const navItems = document.querySelector(".nav-links");
+        const searchIcon = document.querySelector(".fa-search");
+
+        const sectionOne = document.querySelector(".header-section");
 
 
-const sectionOneOptions = {
-    rootMargin: "-625px 0px 0px 0px"
-};
+        const sectionOneOptions = {
+            rootMargin: "-608px 0px 0px 0px"
+        };
 
-const sectionOneObserver = new IntersectionObserver(function(
-    entries,
-    sectionOneObserver
-) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            header.classList.add("sticky-navbar-obs");
-            logo.classList.add("logo-text-obs");
-            navItems.classList.add("nav-links-obs");
-            searchIcon.classList.add("fa-search-obs");
-        } else {
-            header.classList.remove("sticky-navbar-obs");
-            logo.classList.remove("logo-text-obs");
-            navItems.classList.remove("nav-links-obs");
-            searchIcon.classList.remove("fa-search-obs");
+        const sectionOneObserver = new IntersectionObserver(function(
+        entries,
+        sectionOneObserver
+        ) {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting) {
+                    header.classList.add("sticky-navbar-obs");
+                    logo.classList.add("logo-text-obs");
+                    navItems.classList.add("nav-links-obs");
+                    searchIcon.classList.add("fa-search-obs");
+                } else {
+                    header.classList.remove("sticky-navbar-obs");
+                    logo.classList.remove("logo-text-obs");
+                    navItems.classList.remove("nav-links-obs");
+                    searchIcon.classList.remove("fa-search-obs");
         }
     });
-},
+        },
 
-sectionOneOptions);
+        sectionOneOptions);
 
-sectionOneObserver.observe(sectionOne);
+        sectionOneObserver.observe(sectionOne);
+        
+    }
+}
+
+
+
 
 
 
